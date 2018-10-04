f0df5fcf3630ddd8fd1535184a0ec738bb9db732
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-04 00:36:04 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -51,27 +51,6 @@ class Game
     puts "Welcome to Tic Tac Toe!"
   end
 
-  def set_up(input)
-    case num_players
-    when 0
-      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
-      game.play
-    when 1
-      puts "Do you want to be player X and start the match? Y/N"
-      input = gets.strip
-      if input == "Y"
-        game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
-        game.play
-      elsif input == "N"
-        game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
-        game.play
-      end
-    when 2
-      game = Game.new
-      game.play
-    end
-  end
-
   def start
     puts "Ohhh, sounds like an interesting match..."
     puts "Here's the board:"
