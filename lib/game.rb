5ed6a9a65849e4c38dedf4794393800835c598c1
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-04 00:27:40 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -72,7 +72,7 @@ class Game
   def set_up
     puts "How many players are playing this time? 0, 1, or 2?"
     num_players = gets.strip.to_i
-    
+
     case num_players
     when 0
       game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
